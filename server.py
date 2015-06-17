593647b02950c16eb3554bd3ad58af6e91345849
Nick Woodhams <nicholas.woodhams@gmail.com>
2015-06-17 16:18:54 -0400

    changed server config back

--- server.py
+++ server.py
@@ -6,7 +6,6 @@
     autodeploy applications when pushed to git
 """
 
-import os
 import json
 import datetime
 
@@ -101,12 +100,6 @@ def ip_allowed(ip_address):
     return False
 
 
-def demote(user_uid):
-    def result():
-        os.setuid(user_uid)
-    return result
-
-
 # Create your login
 @app.before_first_request
 def create_user():
@@ -185,7 +178,7 @@ def autodeploy():
 
             if application.scriptpath:
                 # Run a script to wrap things up. Change permissions, send an email, whatever
-                Popen('bash %s' % application.scriptpath, preexec_fn=demote(1000), stdout=PIPE, shell=True)
+                Popen('bash %s' % application.scriptpath, shell=True)
 
             status = 'Post-receive hook for %s triggered.' % repo['name']
             print status
